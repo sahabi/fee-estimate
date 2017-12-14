41c373bfc3eaeb2dd8e0984babd65ad051d93ebf
sahabi <sahabi@gmail.com>
2017-12-13 19:54:08 -0600

    implemented currency unit conversion functions

--- src/Units.hs
+++ src/Units.hs
@@ -1,21 +1,44 @@
 module Units
-  ( Bitcoin(..)
+  ( toSAT
+  , toUBTC
+  , toMBTC
+  , toBTC
+  , Bitcoin(..)
   ) where
 
-data Bitcoin = SAT Integer
+data Bitcoin = SAT Double
              | UBTC Double
              | MBTC Double
              | BTC Double
-               deriving(Show)
+               deriving(Show, Arbitrary)
+
+instance Eq Bitcoin where
+  x == y = toSATVal x == (toSATVal y)
+
+instance Ord Bitcoin where
+  x >= y = toSATVal x >= (toSATVal y)
+  x <= y = toSATVal x <= (toSATVal y)
+
+getVal :: Bitcoin -> Double
+getVal (SAT x) = x
+
+toSATVal :: Bitcoin -> Double
+toSATVal = getVal . toSAT
 
 toSAT :: Bitcoin -> Bitcoin
-toSAT = undefined
+toSAT (SAT x) = SAT x
+toSAT (UBTC x) = SAT $ x * 100
+toSAT (MBTC x) = SAT $ x * 100000
+toSAT (BTC x) = SAT $ x * 100000000
 
 toUBTC :: Bitcoin -> Bitcoin
-toUBTC = undefined
+toUBTC (SAT x) = UBTC $ x / 100
+toUBTC x = toUBTC $ toSAT x
 
 toMBTC :: Bitcoin -> Bitcoin
-toMBTC = undefined
+toMBTC (SAT x) = MBTC $ x / 100000
+toMBTC x = toMBTC $ toSAT x
 
 toBTC :: Bitcoin -> Bitcoin
-toBTC = undefined
+toBTC (SAT x) = BTC $ x / 100000000
+toBTC x = toBTC $ toSAT x
